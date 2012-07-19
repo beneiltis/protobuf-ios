b673465c0b17cabe28d26f59eb264b07a01a4e6f
Benedikt Iltisberger <benedikt.iltisberger@orbit.de>
2012-07-19 16:25:33 +0200

    [init] Initial commit of the Xcode project containing the current release of protobug 2.4.1

---
+++ protobuf-ios/protobuf_ios.h
@@ -0,0 +1,13 @@
+//
+//  protobuf_ios.h
+//  protobuf-ios
+//
+//  Created by Benedikt Iltisberger on 19.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface protobuf_ios : NSObject
+
+@end
---
+++ protobuf-ios/protobuf_ios.m
@@ -0,0 +1,13 @@
+//
+//  protobuf_ios.m
+//  protobuf-ios
+//
+//  Created by Benedikt Iltisberger on 19.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "protobuf_ios.h"
+
+@implementation protobuf_ios
+
+@end
